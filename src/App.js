dd85bb6b17f396217b0cf74b48e380d27b1e32c7
Melvin Tom Varghese <m2vcreate@gmail.com>
2023-06-08 11:08:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ImgGenerator from "./Components/ImgGenerator/ImgGenerator";
+
+function App() {
+  return (
+    <>
+      <ImgGenerator />;
+    </>
+  );
+}
+
+export default App;
